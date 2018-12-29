b6c1f29dd22dfefe4ab57d7a09a1db7a8e6bbe56
Felipe Carvalho <felipe.jewish@gmail.com>
2018-12-29 14:57:51 -0200

    Working with Objects, Properties and Methods

--- play.js
+++ play.js
@@ -1,57 +1,10 @@
-const name = "Max";
-let age = 29;
-let hasHobbies = true;
-
-age = 30;
-
-function summarizeUser(userName, userAge, userHasHobby) {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobby
-  );
-}
-
-// 2 another way to write this function above
-// this is an anonymous function
-const summarizeUser1 = function(userName, userAge, userHasHobby) {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobby
-  );
+const person = {
+  name: "Max",
+  age: 29,
+  greet() { //we cannot use arrow function in this case
+    console.log('Hi, I am', this.name)
+  }
 };
 
-// arrow function
-const summarizeUser2 = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobby
-  );
-};
-
-// when we have just 1 statement, we can ommit the reserved word 'return' and the curly braces
-const add = (a, b) => a + b;
-
-// when we have just 1 argument, we can ommit the parenthesis
-const addOne = a => a + 1;
-
-// whe we don't have arguments, we must declare the parenthesis, but them are empty
-const addRandom = () => 1 + 0;
-
-console.log(summarizeUser(name, age, hasHobbies));
-console.log(summarizeUser1(name, age, hasHobbies));
-console.log(summarizeUser2(name, age, hasHobbies));
-console.log(add(1, 2), 'add');
-console.log(addOne(1), 'addOne');
-console.log(addRandom(), 'addRandom');
+console.log(person);
+person.greet();
