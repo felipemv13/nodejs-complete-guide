914655d90a464986bcdd3b2eeed64ed3a2cf1495
Felipe Carvalho <felipe.jewish@gmail.com>
2018-12-30 10:52:56 -0200

    Arrays and arrays methods

--- play.js
+++ play.js
@@ -1,10 +1,23 @@
 const person = {
   name: "Max",
   age: 29,
-  greet() { //we cannot use arrow function in this case
-    console.log('Hi, I am', this.name)
+  greet() {
+    //we cannot use arrow function in this case
+    console.log("Hi, I am", this.name);
   }
 };
 
-console.log(person);
-person.greet();
+const arrays = ["Strings", 123, {}, true];
+const hobbies = ["Sports", "Cooking"];
+
+// for pass trough the array and expose item by item
+for (const hobby of hobbies) {
+  console.log(hobby);
+}
+
+console.log(hobbies);
+console.log(hobbies.map(hobby => hobby));
+// the 2 examples above generate the same result
+
+// i can pass through the array and generate a new array using 'map'
+console.log(hobbies.map(hobby => "Hobby: " + hobby));
